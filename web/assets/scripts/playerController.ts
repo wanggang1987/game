7d8bd53d4a3bbb5a22b1d928f6848b1e40e86ee9
wanggang <mail@ender.wang>
2021-12-12 19:12:06 +0800

    add some code

---
+++ web/assets/scripts/playerController.ts
@@ -0,0 +1,102 @@
+
+import { _decorator, Component, Vec3, systemEvent, SystemEvent, EventMouse, Animation } from 'cc';
+const { ccclass, property } = _decorator;
+
+/**
+ * Predefined variables
+ * Name = PlayerController
+ * DateTime = Sun Dec 12 2021 15:28:36 GMT+0800 (中国标准时间)
+ * Author = ender1111
+ * FileBasename = playerController.ts
+ * FileBasenameNoExtension = playerController
+ * URL = db://assets/scripts/playerController.ts
+ * ManualUrl = https://docs.cocos.com/creator/3.3/manual/zh/
+ *
+ */
+ 
+@ccclass('PlayerController')
+export class PlayerController extends Component {
+    // for fake tween
+    // 是否接收到跳跃指令
+    private _startJump: boolean = false;
+    // 跳跃步长
+    private _jumpStep: number = 0;
+    // 当前跳跃时间
+    private _curJumpTime: number = 0;
+    // 每次跳跃时常
+    private _jumpTime: number = 0.1;
+    // 当前跳跃速度
+    private _curJumpSpeed: number = 0;
+    // 当前角色位置
+    private _curPos: Vec3 = new Vec3();
+    // 每次跳跃过程中，当前帧移动位置差
+    private _deltaPos: Vec3 = new Vec3(0, 0, 0);
+    // 角色目标位置
+    private _targetPos: Vec3 = new Vec3();
+
+    @property({type: Animation})
+    public BodyAnim: Animation | null = null;
+
+    start () {
+        // Your initialization goes here.
+        systemEvent.on(SystemEvent.EventType.MOUSE_UP, this.onMouseUp, this);
+    }
+
+    onMouseUp(event: EventMouse) {
+        if (event.getButton() === 0) {
+            this.jumpByStep(1);
+        }
+        else if (event.getButton() === 2) {
+            this.jumpByStep(2);
+        }
+    }
+
+    jumpByStep(step: number) {
+        if (this._startJump) {
+            return;
+        }
+
+        if (this.BodyAnim) {
+             if (step === 1) {
+                 this.BodyAnim.play('onestep');
+             } else if (step === 2) {
+                 this.BodyAnim.play('twostep');
+             }
+         }        
+
+        this._startJump = true;
+        this._jumpStep = step;
+        this._curJumpTime = 0;
+        this._curJumpSpeed = this._jumpStep / this._jumpTime;
+        this.node.getPosition(this._curPos);
+        Vec3.add(this._targetPos, this._curPos, new Vec3(this._jumpStep, 0, 0));
+    }
+
+    update (deltaTime: number) {
+        if (this._startJump) {
+            this._curJumpTime += deltaTime;
+            if (this._curJumpTime > this._jumpTime) {
+                // end
+                this.node.setPosition(this._targetPos);
+                this._startJump = false;
+            } else {
+                // tween
+                this.node.getPosition(this._curPos);
+                this._deltaPos.x = this._curJumpSpeed * deltaTime;
+                Vec3.add(this._curPos, this._curPos, this._deltaPos);
+                this.node.setPosition(this._curPos);
+            }
+        }
+    }
+}
+
+/**
+ * [1] Class member could be defined like this.
+ * [2] Use `property` decorator if your want the member to be serializable.
+ * [3] Your initialization goes here.
+ * [4] Your update function goes here.
+ *
+ * Learn more about scripting: https://docs.cocos.com/creator/3.3/manual/zh/scripting/
+ * Learn more about CCClass: https://docs.cocos.com/creator/3.3/manual/zh/scripting/ccclass.html
+ * Learn more about life-cycle callbacks: https://docs.cocos.com/creator/3.3/manual/zh/scripting/life-cycle-callbacks.html
+ */
