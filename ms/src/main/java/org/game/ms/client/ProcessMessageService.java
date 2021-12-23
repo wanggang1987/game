faf9ecdb2428da7f3a6d39bad581b1d99b527e57
wanggang <mail@ender.wang>
2021-12-23 22:11:46 +0800

    发布一个web版本

---
+++ ms/src/main/java/org/game/ms/client/ProcessMessageService.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.game.ms.client;
+
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author gangwang
+ */
+@Service
+public class ProcessMessageService {
+    
+}
