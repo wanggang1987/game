adc667bf52034c8e7b3a3bf9914fdf05b2ba1e58
wanggang <mail@ender.wang>
2021-12-27 22:45:28 +0800

    a new web release

--- ms/reboot.sh
+++ ms/reboot.sh
@@ -3,6 +3,10 @@ kill -9 `ps -ef | grep ms | grep -v grep | awk '{print $2}'`
 echo "git pull"
 git pull
 sleep 1
+echo "------> rm -rf ./src/main/resources/static/*"
+rm -rf ./src/main/resources/static/*
+echo "------> cp -rf ../cocos2.4.7/build/web-mobile/*  ./src/main/resources/static/."
+cp -rf ../cocos2.4.7/build/web-mobile/*  ./src/main/resources/static/.
 echo "nohup mvn clean spring-boot:run >> log &"
 nohup mvn clean spring-boot:run >> log &
 echo "tail -f log"
